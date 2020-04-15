6746089f35ee757cbadc82075aaf550a60bd8324
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2020-04-15 14:41:59 -0400

    Ex09 | Complete newshound

--- exercises/ex09/newshound.c
+++ exercises/ex09/newshound.c
@@ -13,7 +13,10 @@ Modified by Allen Downey.
 #include <sys/types.h>
 #include <wait.h>
 
-
+/*The program only prints out the first feed because execle() function will
+* terminate the program once it's done.
+*
+*/
 void error(char *msg)
 {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
@@ -36,16 +39,40 @@ int main(int argc, char *argv[])
         "http://www.nytimes.com/services/xml/rss/nyt/AsiaPacific.xml"
     };
     int num_feeds = 5;
+    int i;
+    int status;
     char *search_phrase = argv[1];
     char var[255];
+    pid_t pid;
 
     for (int i=0; i<num_feeds; i++) {
+      pid = fork();
+
+      /* check for an error */
+      if (pid == -1) {
+          fprintf(stderr, "fork failed: %s\n", strerror(errno));
+          perror(argv[0]);
+          exit(1);
+      }
+      if (!pid) {
         sprintf(var, "RSS_FEED=%s", feeds[i]);
         char *vars[] = {var, NULL};
 
         int res = execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars);
         if (res == -1) {
             error("Can't run script.");
+        }
+      }
+    }
+
+    // Wait for child processes
+    for (i=0; i<num_feeds; i++) {
+        pid = wait(&status);
+
+        if (pid == -1) {
+            fprintf(stderr, "wait failed: %s\n", strerror(errno));
+            perror(argv[0]);
+            exit(1);
         }
     }
     return 0;
